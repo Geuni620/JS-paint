97cbf52578aaba788b28600b85a05b395c98384b
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-02-01 19:43:24 +0900

    paintJS context

---
+++ app.js
@@ -0,0 +1,46 @@
+const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+
+//색상이나 스타일을 설정
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+//캔버스 위 마우스 인지
+function onMouseMove(event) {
+  const X = event.offsetX;
+  const Y = event.offsetY;
+
+  if (!painting) {
+    console.log("creating Path in", X, Y);
+    ctx.beginPath();
+    ctx.moveTo(X, Y);
+  } else {
+    console.log("creating line in", X, Y);
+    ctx.lineTo(X, Y);
+    ctx.stroke();
+  }
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
